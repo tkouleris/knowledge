aa97615dcda35153d33ae8cce934e1dff55603ea
darkheart101 <kurokokoro@windowslive.com>
2021-02-14 20:10:48 +0200

    chore: video table migration

---
+++ database/migrations/2021_02_14_180938_create_video_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('knowledge_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video');
+    }
+}
