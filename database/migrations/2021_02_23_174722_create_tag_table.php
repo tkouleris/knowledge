d9a1300ac33fda6a2ee9e20171d27e8bdde7dbec
darkheart101 <kurokokoro@windowslive.com>
2021-02-23 19:48:41 +0200

    chore: tag migration file

---
+++ database/migrations/2021_02_23_174722_create_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag');
+            $table->foreignId('user_id')->default(0)->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag');
+    }
+}
