0d1be8445af4ad2e728b5a7e107dbb57071ef967
darkheart101 <kurokokoro@windowslive.com>
2021-03-22 20:43:08 +0200

    chore: cleanup

--- resources/views/knowledgeapp.blade.php
+++ resources/views/knowledgeapp.blade.php
@@ -9,21 +9,15 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
 </head>
 <div id="app">
     <App></App>
 </div>
 
-<!-- jQuery -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-{{--<script src="plugins/jquery/jquery.min.js"></script>--}}
-<!-- Bootstrap 4 -->
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- AdminLTE App -->
 <script src="{{ asset('js/adminlte.min.js') }}"></script>
 
 </html>
