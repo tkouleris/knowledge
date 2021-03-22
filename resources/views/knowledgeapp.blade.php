39fa39220ed919e7d3d4c52f720992f7eaa68d19
darkheart101 <kurokokoro@windowslive.com>
2021-03-22 20:31:11 +0200

    chore: front - cleanup login page

--- resources/views/knowledgeapp.blade.php
+++ resources/views/knowledgeapp.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in (v2)</title>
+    <title>Knowledge</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -18,8 +18,6 @@
     <App></App>
 </div>
 
-
-
 <!-- jQuery -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 {{--<script src="plugins/jquery/jquery.min.js"></script>--}}
@@ -27,5 +25,5 @@
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('js/adminlte.min.js') }}"></script>
-</body>
+
 </html>
